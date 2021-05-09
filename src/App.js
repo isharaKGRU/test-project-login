81044e89a3b651ac96a2dec3704c2b3464cae529
ishara.k.g.r.u <48258158+isharaKGRU@users.noreply.github.com>
2021-05-09 12:50:05 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Home from './components/Home';
+import Signin from './components/Signin';
+import Signun from './components/Signup';
+import { auth } from './firebase';
+
+
+function App() {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      const user = {
+        uid: userAuth?.uid,
+        email: userAuth?.email
+      }
+      if (userAuth) {
+        console.log(userAuth)
+        setUser(user)
+      } else {
+        setUser(null)
+      }
+    })
+    return unsubscribe
+  }, [])
+  return (
+    <div className="App">
+      {user ? <Home /> : <Signup />}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyA384wefks7NkXKZ4HIS0DW3PzJ3zydSLo",
+    authDomain: "fir-login-925a5.firebaseapp.com",
+    projectId: "fir-login-925a5",
+    storageBucket: "fir-login-925a5.appspot.com",
+    messagingSenderId: "273581573478",
+    appId: "1:273581573478:web:a9e16be7376abba797f4a7",
+    measurementId: "G-C2EECCVSRF"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+  export {auth};
+  export default db;
+  firebase.analytics();
+
